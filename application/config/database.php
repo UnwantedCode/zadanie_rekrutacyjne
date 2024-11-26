236de86b75e13d34f1293d24fb5e554fd75e7485
adamtarkowski <adam@dharsa.com>
2024-11-26 22:15:41 +0100

    change some

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'rekrutacja_mysql',
+	'username' => 'user',
+	'password' => 'passwd',
+	'database' => 'example_db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
